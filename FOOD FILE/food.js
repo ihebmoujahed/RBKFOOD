ef97ae239f4a80b7e3e307c528a2c812f8816c79
mjhdiheb02@gmail.com <mjhdiheb02@gmail.com>
2021-10-09 21:51:02 +0100

    last

--- FOOD FILE/food.js
+++ FOOD FILE/food.js
@@ -1,26 +1,26 @@
 function tayesh(){
-    var url = "http://google.com";
+    var url = "file:///C:/Users/INFOKOM/Desktop/RBKFOOD/pizza/pizza.html";
 $(location).attr('href',url);
  
     
         return 
 }
 function tayesh1(){
-    var url = "http://google.com";
+    var url = "file:///C:/Users/INFOKOM/Desktop/RBKFOOD/malfouf/malfouf.html";
 $(location).attr('href',url);
  
     
         return 
 }
 function tayesh2(){
-    var url = "http://google.com";
+    var url = "file:///C:/Users/INFOKOM/Desktop/RBKFOOD/ma9loub/ma9loub.html";
 $(location).attr('href',url);
  
     
         return 
 }            
 function tayesh3(){
-    var url = "http://google.com";
+    var url = "file:///C:/Users/INFOKOM/Desktop/RBKFOOD/baguettefarcie/baguettefarcie.html";
 $(location).attr('href',url);
  
     
