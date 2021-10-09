b51476ad36ae70cfbec3744af261ac0f2d0fcf7b
mjhdiheb02@gmail.com <mjhdiheb02@gmail.com>
2021-10-09 20:14:45 +0100

    6steap

---
+++ FOOD FILE/food.js
@@ -0,0 +1,29 @@
+function tayesh(){
+    var url = "http://google.com";
+$(location).attr('href',url);
+ 
+    
+        return 
+}
+function tayesh1(){
+    var url = "http://google.com";
+$(location).attr('href',url);
+ 
+    
+        return 
+}
+function tayesh2(){
+    var url = "http://google.com";
+$(location).attr('href',url);
+ 
+    
+        return 
+}            
+function tayesh3(){
+    var url = "http://google.com";
+$(location).attr('href',url);
+ 
+    
+        return 
+}       
+        
