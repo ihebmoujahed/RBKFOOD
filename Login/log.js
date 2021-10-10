27416de97fd1ac0c182490eddf98b5508ea99abc
mjhdiheb02@gmail.com <mjhdiheb02@gmail.com>
2021-10-11 00:14:31 +0100

    end

--- Login/log.js
+++ Login/log.js
@@ -2,3 +2,8 @@ function loog(){
     var url = "file:///C:/Users/INFOKOM/Desktop/RBKFOOD/FOOD%20FILE/food.html";
 $(location).attr('href',url);
 }
+
+function HOME(){
+    var url = "file:///C:/Users/INFOKOM/Desktop/RBKFOOD/Home/Home.html";
+$(location).attr('href',url);
+}
