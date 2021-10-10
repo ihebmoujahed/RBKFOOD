daeb46175191f4e65bdb2724f8c59b83f890b5df
mjhdiheb02@gmail.com <mjhdiheb02@gmail.com>
2021-10-10 11:48:45 +0100

    edit

--- Home/HOME.js
+++ Home/HOME.js
@@ -1,23 +1,26 @@
-$("#in1").hide()
-$("#in2").hide()
-$("#in3").hide()
-$("#in4").hide()
-$("label").hide()
+$("#in1").hide(200)
+$("#in2").hide(200)
+$("#in3").hide(200)
+$("#in4").hide(200)
+$("label").hide(200)
 $("bo2").hide(200)
+$("#bo3").hide(200)
 
 function kamouniya(){
 if($('#bo2').hide(200)){
+    $('#bo1').hide(200)
     $("#in1").show(200)
     $("#in2").show(200)
     $("#in3").show(200)
     $("#in4").show(200)
     $("label").show(200)
+    $("#bo3").show(200)
 
     return
 }
 }
 
 function pageoff(){
-    var url = "file:///C:/Users/INFOKOM/Desktop/RBKFOOD/FOOD%20FILE/food.html";
+    var url = "file:///C:/Users/INFOKOM/Desktop/RBKFOOD/Login/Login.html";
 $(location).attr('href',url);
 }
